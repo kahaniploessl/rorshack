a410a5302f6467445d2e45076cf2aad12bb9b392
kahaniploessl <46905494+kahaniploessl@users.noreply.github.com>
2019-11-22 18:38:12 -0500

    Add files via upload

---
+++ rorshack-2/README.markdown
@@ -0,0 +1,4 @@
+# rorshack
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/kahaniploessl/pen/VwwNBgp](https://codepen.io/kahaniploessl/pen/VwwNBgp).
+
+ 
---
+++ rorshack-2/src/script.js
@@ -0,0 +1,11 @@
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+}
+
+function draw() {
+  frameRate(15);
+  rotateX(frameCount*0.005);
+  rotateY(frameCount*0.005);
+  box(80,200);
+}
