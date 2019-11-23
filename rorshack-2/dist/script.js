0a183d4d3f8ae1c558735467fecefb7f58e69570
kahaniploessl <46905494+kahaniploessl@users.noreply.github.com>
2019-11-22 19:38:42 -0500

    Add files via upload

--- rorshack-2/dist/script.js
+++ rorshack-2/dist/script.js
@@ -1,10 +1,30 @@
+
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
 }
 
+let c = 0;
+let x = 20;
+let y = 700;
+let z = 500;
+
 function draw() {
-  frameRate(15);
+  frameRate(60);
   rotateX(frameCount*0.02);
   rotateY(frameCount*0.02);
-  box(160,600);
+  box(x,y,z);
+  //box(200)
+  c++;
+  console.log(c);
+
+  if (c%1500==0){
+    background(255);
+    x=random(10,50);
+    y=random(500,700);
+    z=random(500,700);
+  }
+  // rotateX(frameCount*0.2);
+  // rotateY(frameCount*0.2);
+  // box(20,400,400);
+  //test
 }
